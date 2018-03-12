47d6dafccae7351507b05abb5974c3367c57fc01
Nathaniel Jansen <nathanieljansen@Nathaniels-MacBook-Pro.local>
2018-03-12 17:03:14 -0600

    database is up and working

--- javascript/app.js
+++ javascript/app.js
@@ -7,3 +7,34 @@ var config = {
   messagingSenderId: "1070050230140"
 };
 firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var trainName = "";
+var destination = "";
+var trainTime = "";
+var frequency = "";
+
+$("#add-train").on("click", function () {
+  alert("I was clicked")
+  event.preventDefault();
+  trainName = $("#train-input").val().trim();
+  destination = $("#destination-input").val().trim();
+  trainTime = $("#time-input").val().trim();
+  frequency = $("#frequency-input").val().trim();
+  database.ref().set({
+    trainName: trainName,
+    destination: destination,
+    trainTime: trainTime,
+    frequency: frequency,
+
+  });
+});
+
+database.ref().on("value", function (snapshot) {
+  console.log(snapshot.val());
+  console.log(snapshot.val().trainName);
+  console.log(snapshot.val().destination);
+  console.log(snapshot.val().trainTime);
+  console.log(snapshot.val().frequency);
+});
