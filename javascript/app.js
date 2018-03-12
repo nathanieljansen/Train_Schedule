b8abdaa2d6f10509986e3d6d346793a91b41af81
Nathaniel Jansen <nathanieljansen@Nathaniels-MacBook-Pro.local>
2018-03-12 16:01:31 -0600

    basic layout

---
+++ javascript/app.js
@@ -0,0 +1,9 @@
+var config = {
+  apiKey: "AIzaSyCaKdTcPkr96n9jc0k9awI23KdXr60L2hs",
+  authDomain: "train-schedule-30eeb.firebaseapp.com",
+  databaseURL: "https://train-schedule-30eeb.firebaseio.com",
+  projectId: "train-schedule-30eeb",
+  storageBucket: "",
+  messagingSenderId: "1070050230140"
+};
+firebase.initializeApp(config);
